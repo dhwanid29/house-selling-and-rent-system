1a3ee9abef2c8bf3c86528211e6dafdcef411c8e
dhwani <dhwanid.inexture@gmail.com>
2022-06-10 13:34:35 +0530

    Added Validations

--- house_selling/settings.py
+++ house_selling/settings.py
@@ -13,7 +13,8 @@ import os
 from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
-
+# import django_heroku
+# django_heroku.settings(locals())
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$m#k72#2#jbiujhv7qs)_gf)92@v(91^lgg40mz2h)v-k=t6(p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['house-selling-and-rent-system.herokuapp.com', '127.0.0.1:8000']
 
@@ -81,13 +82,6 @@ WSGI_APPLICATION = 'house_selling.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Database
