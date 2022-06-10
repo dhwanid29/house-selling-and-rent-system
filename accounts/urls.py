5845a3d5e752831383680f59011c71c8ded6a75c
dhwani <dhwanid.inexture@gmail.com>
2022-06-10 15:43:31 +0530

    Change Password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import UserRegistrationView, UserLoginView
+from .views import UserRegistrationView, UserLoginView, UserChangePasswordView
     # , UserProfileView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
-    # path('profile/', UserProfileView.as_view(), name='profile')
+    # path('profile/', UserProfileView.as_view(), name='profile'),
+    path('changepassword/', UserChangePasswordView.as_view(), name='changepassword'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from .renderers import UserRenderer
-from .serializers import UserRegistrationSerializer, UserLoginSerializer
+from .serializers import UserRegistrationSerializer, UserLoginSerializer, UserChangePasswordSerializer
     # , UserProfileSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 
@@ -24,6 +24,7 @@ class UserRegistrationView(APIView):
     def post(self, request, format=None):
         print("request.data", request.data, type(request.data))
         serializer = UserRegistrationSerializer(data=request.data)
+        user = self.context.get('user')
         if serializer.is_valid(raise_exception=True):
             user = serializer.save()
             token = get_tokens_for_user(user)
@@ -51,3 +52,12 @@ class UserLoginView(APIView):
 #     def get(self, request, format=None):
 #         serializer = UserProfileSerializer(request.user)
 #         return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UserChangePasswordView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, format=None):
+        serializer = UserChangePasswordSerializer(data=request.data, context={'user': request.user})
+        if serializer.is_valid(raise_exception=True):
+            return Response({'msg': 'Password Changed Successfully!'}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
