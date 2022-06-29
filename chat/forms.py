88868025a15861ca9e4b0b8a08fe4d27a1558dc7
dhwani <dhwanid.inexture@gmail.com>
2022-06-29 18:22:15 +0530

    Fully Completed Chat Feature

--- chat/forms.py
+++
@@ -1,8 +0,0 @@
-# from django.forms import forms
-# from chat.models import Message
-#
-#
-# class MessageForm(forms.ModelForm):
-#     class Meta:
-#         model = Message
-#         fields = ['receiver']
